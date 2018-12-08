0487ec286be04648896d8382afacece9a0133769
Neil Smith <trolly999@gmail.com>
2018-12-08 17:06:09 +0000

    day7 part2

--- src/Day07.kt
+++ src/Day07.kt
@@ -7,6 +7,8 @@ class Day07 {
 
     val v = mutableSetOf<Char>()
     val e = mutableSetOf<Pair<Char, Char>>()
+    val w = mutableListOf<Int>()
+    val weights = mutableMapOf<Char, Int>()
 
     fun loadData(filename: String) {
         File(filename).readLines().forEach {
@@ -18,16 +20,26 @@ class Day07 {
         }
     }
 
-    fun getCandidates(): List<Char> {
+    fun getCandidateEdges(): List<Char> {
         val targets = e.map { it.second }.toSet()
         val sources = e.map { it.first }.toSet().filter { v.contains(it) }
         return if (e.isEmpty()) v.toList() else (sources - targets).sorted().toList()
     }
 
+    fun calculateMinWeights(order: String, unitTime: Int) {
+        order.forEach {
+            val minOfPrevious = e.filter { edge -> edge.second == it }
+                .map { edge -> (weights[edge.first] ?: 0) + edge.first.toInt() - 'A'.toInt() + unitTime + 1 }
+                .max() ?: 0
+            weights[it] = minOfPrevious
+        }
+
+    }
+
     fun process(): String {
         var result = ""
         while (v.isNotEmpty()) {
-            val it = getCandidates().get(0)
+            val it = getCandidateEdges()[0]
             result += it
             e.removeAll { pair -> pair.first == it }
             v.remove(it)
@@ -35,6 +47,39 @@ class Day07 {
         return result
     }
 
+
+    fun buildIt(numberOfWorkers: Int, unitTime: Int, order: String): Int {
+        (1..numberOfWorkers).forEach { _ -> w.add(0) }
+        calculateMinWeights(order, unitTime)
+
+        while (v.isNotEmpty()) {
+            val candidates = getCandidateEdges()
+            val indecesOfAvailableWorkers = getIndecesOfAvailableWorkers(candidates.size)
+
+            var minValueOfNew = Int.MAX_VALUE
+
+            (0 until indecesOfAvailableWorkers.size).forEach {
+                val workerIndex = indecesOfAvailableWorkers[it]
+                if (w[workerIndex] < weights.get(candidates[it]) ?: 0) w[workerIndex] = weights[candidates[it]] ?: 0
+                w[workerIndex] = w[workerIndex] + candidates[it].toInt() - 'A'.toInt() + unitTime + 1
+                if (w[workerIndex] < minValueOfNew) minValueOfNew = w[workerIndex]
+                e.removeAll { pair -> pair.first == candidates[it] }
+                v.remove(candidates[it])
+            }
+
+            w.indices.forEach {
+                if (w[it] < minValueOfNew && !indecesOfAvailableWorkers.contains(it)) w[it] = minValueOfNew
+            }
+        }
+
+        return w.max() ?: 0
+    }
+
+    private fun getIndecesOfAvailableWorkers(size: Int): List<Int> {
+        val minWorker = w.min()
+        val possibleWorkers = w.indices.filter { w[it] == minWorker }
+        return possibleWorkers.subList(0, minOf(size, possibleWorkers.size))
+    }
 }
 
 class Day07Test {
@@ -44,6 +89,24 @@ class Day07Test {
     @Before
     fun setUp() {
         day7 = Day07()
+    }
+
+    @Test
+    fun buildItBig() {
+        day7.loadData("Data/Day07/day7-big.txt")
+        assertEquals(1053, day7.buildIt(5, 60, "FMOXCDGJRAUIHKNYZTESWLPBQV"))
+    }
+
+    @Test
+    fun buildItSmall2() {
+        day7.loadData("Data/Day07/day7-small.txt")
+        assertEquals(14, day7.buildIt(3, 0, "CABDFE"))
+    }
+
+    @Test
+    fun buildItSmall() {
+        day7.loadData("Data/Day07/day7-small.txt")
+        assertEquals(15, day7.buildIt(2, 0, "CABDFE"))
     }
 
     @Test
@@ -61,7 +124,7 @@ class Day07Test {
     @Test
     fun checkGetCandidates() {
         day7.loadData("Data/Day07/day7-small.txt")
-        assertEquals(listOf('C'), day7.getCandidates())
+        assertEquals(listOf('C'), day7.getCandidateEdges())
     }
 
     @Test
