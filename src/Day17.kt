b6a92a3a90bbb8d7d2f9296f0d83f187f85b5beb
Neil Smith <neil.smith2@ee.co.uk>
2019-01-02 15:56:41 +0000

    day 17 finished

--- src/Day17.kt
+++ src/Day17.kt
@@ -18,6 +18,8 @@ class Day17 {
 
     var groundMap = arrayListOf<Array<GroundType>>()
     var xShift = 0
+    var minInputY = Int.MAX_VALUE
+    var maxInputY = 0
 
     val coords: Deque<Coord> = ArrayDeque()
 
@@ -26,6 +28,7 @@ class Day17 {
             row.forEach { sq -> print(sq.printCharacter) }
             println()
         }
+        println()
     }
 
     private fun xValue(x: Int): Int = x - xShift
@@ -52,6 +55,11 @@ class Day17 {
         xShift = (ranges.minBy { r -> r.first.start }?.first?.start ?: 0) - 1
         val maxX = (ranges.maxBy { r -> r.first.endInclusive }?.first?.endInclusive ?: 0) + 2
 
+
+        if (maxY > maxInputY) maxInputY = maxY
+        val minY = ranges.minBy { r -> r.second.start }?.second?.start ?: Int.MAX_VALUE
+        if (minY < minInputY) minInputY = minY
+
         repeat(maxY + 1) {
             groundMap.add(Array(xValue(maxX)) { GroundType.SAND })
         }
@@ -69,70 +77,88 @@ class Day17 {
 
     fun processData() {
         coords.add(Coord(xValue(500), 0))
+//        var count = 0
 
         fun addPoint(x: Int, y: Int) {
-            if (y < groundMap.size-1) {
+            if (y < groundMap.size - 1) {
                 coords.add(Coord(x, y))
             }
         }
         while (coords.isNotEmpty()) {
-//            printGrid()
-//            println()
+//            if (count++ == 500) {
+//                printGrid()
+//            }
             val point = coords.pop()
             val nextPoint = Coord(point.x, point.y + 1)
             when (groundMap[nextPoint.y][nextPoint.x]) {
 
-                GroundType.CLAY, GroundType.WATER, GroundType.WET_SAND -> {
+                GroundType.CLAY, GroundType.WATER -> {
                     val maxLeft = goLeft(point)
                     val maxRight = goRight(point)
 
-                    if (groundMap[maxLeft.y + 1][maxLeft.x] == GroundType.SAND || groundMap[maxRight.y + 1][maxRight.x] == GroundType.SAND) {
+                    if (groundMap[maxLeft.y + 1][maxLeft.x] == GroundType.SAND || groundMap[maxLeft.y + 1][maxLeft.x] == GroundType.WET_SAND ||
+                        groundMap[maxRight.y + 1][maxRight.x] == GroundType.SAND || groundMap[maxRight.y + 1][maxRight.x] == GroundType.WET_SAND
+                    ) {
                         if (groundMap[maxLeft.y + 1][maxLeft.x] == GroundType.SAND) {
                             coords.add(maxLeft)
+                            groundMap[maxLeft.y][maxLeft.x] = GroundType.WET_SAND
                         }
                         if (groundMap[maxRight.y + 1][maxRight.x] == GroundType.SAND) {
                             coords.add(maxRight)
+                            groundMap[maxRight.y][maxRight.x] = GroundType.WET_SAND
                         }
-                        for (i in maxLeft.x..maxRight.x) {
+                        for (i in maxLeft.x + 1 until maxRight.x) {
                             groundMap[maxLeft.y][i] = GroundType.WET_SAND
                         }
                     } else {
-                        for (i in maxLeft.x..maxRight.x) {
+                        for (i in maxLeft.x + 1 until maxRight.x) {
                             groundMap[maxLeft.y][i] = GroundType.WATER
                         }
+//                        if (groundMap[maxLeft.y][maxLeft.x] == GroundType.CLAY && groundMap[maxLeft.y][maxLeft.x] == GroundType.CLAY) {
                         addPoint(point.x, point.y - 1)
+//                        }
                     }
                 }
                 GroundType.SAND -> {
                     groundMap[nextPoint.y][nextPoint.x] = GroundType.WET_SAND
                     addPoint(nextPoint.x, nextPoint.y)
                 }
-                GroundType.SPRING -> {
+                GroundType.SPRING, GroundType.WET_SAND -> {
                 }
             }
         }
     }
 
-    private fun goLeft(nextPoint: Coord): Coord {
-        var x = nextPoint.x
+    private fun goLeft(point: Coord): Coord {
+        var x = point.x
 
-        while (groundMap[nextPoint.y][x - 1] != GroundType.CLAY && groundMap[nextPoint.y + 1][x] != GroundType.SAND) {
+        do {
             x--
-        }
-        return Coord(x, nextPoint.y)
+        } while ((groundMap[point.y][x] == GroundType.SAND || groundMap[point.y][x] == GroundType.WET_SAND) && (groundMap[point.y + 1][x] == GroundType.CLAY || groundMap[point.y + 1][x] == GroundType.WATER))
+
+        return Coord(x, point.y)
     }
 
-    private fun goRight(nextPoint: Coord): Coord {
-        var x = nextPoint.x
+    private fun goRight(point: Coord): Coord {
+        var x = point.x
 
-        while (groundMap[nextPoint.y][x + 1] != GroundType.CLAY && groundMap[nextPoint.y + 1][x] != GroundType.SAND) {
+        do {
             x++
-        }
-        return Coord(x, nextPoint.y)
+        } while ((groundMap[point.y][x] == GroundType.SAND || groundMap[point.y][x] == GroundType.WET_SAND) && (groundMap[point.y + 1][x] == GroundType.CLAY || groundMap[point.y + 1][x] == GroundType.WATER))
+
+        return Coord(x, point.y)
     }
 
-    fun countWater() : Int {
-        return groundMap.sumBy { y -> y.filter { x -> x == GroundType.WATER || x == GroundType.WET_SAND }.count() }
+    fun countWater(): Int {
+        return (minInputY..maxInputY)
+            .map { y -> groundMap[y] }
+            .sumBy { x -> x.filter { v -> v == GroundType.WATER || v == GroundType.WET_SAND }.count() }
+    }
+
+    fun countJustWater(): Int {
+        return (minInputY..maxInputY)
+            .map { y -> groundMap[y] }
+            .sumBy { x -> x.filter { v -> v == GroundType.WATER }.count() }
     }
 }
 
@@ -151,11 +177,29 @@ class Day17Test {
     }
 
     @Test
+    fun runBigPart2() {
+        day17.loadData("Data/Day17/day17-input.txt")
+        day17.processData()
+        day17.printGrid()
+        assertEquals(27086, day17.countJustWater())
+        // 34780 too big
+    }
+
+    @Test
+    fun check2ProcessSmall() {
+        day17.loadData("Data/Day17/day17-small.txt")
+        day17.processData()
+        day17.printGrid()
+        assertEquals(29, day17.countJustWater())
+    }
+
+    @Test
     fun runBigPart1() {
         day17.loadData("Data/Day17/day17-input.txt")
         day17.processData()
         day17.printGrid()
-        assertEquals(57,day17.countWater())
+        assertEquals(34775, day17.countWater())
+        // 34780 too big
     }
 
     @Test
@@ -163,7 +207,18 @@ class Day17Test {
         day17.loadData("Data/Day17/day17-small.txt")
         day17.processData()
         day17.printGrid()
-        assertEquals(57,day17.countWater())
+        assertEquals(57, day17.countWater())
+    }
+
+    @Test
+    fun checkLoadTest1() {
+        day17.loadData("Data/Day17/day17-test-1.txt")
+        day17.printGrid()
+//        assertEquals(499, day17.xShift)
+//        assertEquals(8, day17.groundMap.size)
+        day17.processData()
+        day17.printGrid()
+        assertEquals(18, day17.countWater())
     }
 
     @Test
